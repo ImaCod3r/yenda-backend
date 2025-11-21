be6f33989b768098ce27a6a1c9cbf4261334fa99
Edson Rodrigues <thecodeverse12@gmail.com>
2025-11-21 20:09:31 +0100

    removing api key verification

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import './models/index.js';
 import routes from "./routes/index.js";
-import { verifyApiKey } from "./middlewares/apiAuth.js";
 
 const app = express();
 
@@ -11,12 +10,11 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cors({
   origin: true,
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "x-secret-key"]
 }));
 app.use(cookieParser());
 
 // Routes
-app.use("/api", verifyApiKey, routes);
+app.use("/api", routes);
 app.get("/", (_, res) => {
   res.json({ "message": "Welcome to Yenda API" });
 });
